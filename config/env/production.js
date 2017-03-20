38a4e6cee9cfe8317079d39120ef890fad46125a
Brian Stanley <Brian Stanley>
2017-03-20 16:30:54 -0400

    deployment

--- config/env/production.js
+++ config/env/production.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: 'mongodb://shoppinglist:Shoppinglist123@ds137090.mlab.com:37090/bjt',
     options: {
       user: '',
       pass: ''
